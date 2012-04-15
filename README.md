8cf985777b5baafd3722e17b23b23f2070b50682
Matias Niemela <matias@yearofmoo.com>
2012-04-14 22:03:03 -0400

    README + install file updated

---
+++ README.md
@@ -0,0 +1,7 @@
+# Git-Concat
+
+install with:
+
+```
+curl https://raw.github.com/matsko/Git-Concat/master/install.sh | bash
+```
--- install.sh
+++ install.sh
@@ -1,25 +1,35 @@
 #!/usr/bin/env bash
 
 DIR="./Git-Concat"
+FILE="$DIR/pre-commit.rb"
+FINAL="./.git/hooks/pre-commit"
 
-if [ ! -d ./.git ]
+if [ ! -d ./.git ]; then
   echo "Git-Concat: Git repo in this directory is non existant"
-  exit 1
+  exit 0
+fi
+
+if [ -f $FINAL ]; then
+  echo "Git-Concat: pre-commit file already exists in $FINAL"
+  exit 0
 fi
 
 git clone git://github.com/matsko/Git-Concat.git $DIR
 
-FILE="$DIR/pre-commit.rb"
-
-if [ ! -f $FILE ]
+if [ ! -f $FILE ]; then
   echo "Git-Concat: Git pull operation failed"
-  exit 1
+  exit 0
 fi
 
 chmod +x ./$FILE
 mv ./$FILE ./.git/hooks/pre-commit
 
 rm -fr $DIR
+
+if [ ! -f $FILE ]; then
+  echo "Git-Concat: Git pull operation failed"
+  exit 0
+fi
 
 touch "./.gitconcat"
 
